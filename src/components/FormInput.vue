48a67d5b7bcd41e2c7ed9f318346d66b1a9d3270
Max <v2asault@yahoo.com.tw>
2022-08-12 22:07:37 +0800

    add forminput component

---
+++ src/components/FormInput.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+defineProps<{
+  name: string
+  modelValue: string
+}>()
+
+const emit = defineEmits<
+  {(event: 'update:modelValue', value: string): void }
+>()
+
+function handleInput (e: Event) {
+  const value = (e.target as HTMLInputElement).value
+  emit('update:modelValue', value)
+}
+</script>
+
+<template>
+  <div>
+    <label :for="name">{{ name }}</label>
+    <div>
+      <input
+        :id="name"
+        type="text"
+        :value="modelValue"
+        @input="handleInput"
+      >
+    </div>
+  </div>
+</template>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,10 +1,18 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import { useModal } from '@/composables/modal'
+import FormInput from '@/components/FormInput.vue'
 
 const modal = useModal()
+
+const username = ref('')
 </script>
 
 <template>
+  <FormInput
+    v-model="username"
+    name="username"
+  ></FormInput>
   <div class="flex justify-end items-center">
     <button
       class="mr-2"
@@ -13,7 +21,7 @@ const modal = useModal()
       Sign up
     </button>
     <button
-      class=" p-2 border border-dark-400 rounded cursor-pointer transform duration-300 hover:scale-105 active:translate-y-1"
+      class="p-2 border border-dark-400 rounded cursor-pointer transform duration-300 hover:scale-105 active:translate-y-1"
     >
       <router-link
         to="/post/new"
